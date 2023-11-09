fe0850246ef463e727eb82897090de5d211b98fa
CampanelliEmanuele <emanuele.campanelli2001@gmail.com>
2023-11-09 14:24:17 +0100

    Bonus 1 completed

--- js/main.js
+++ js/main.js
@@ -6,3 +6,7 @@ let color = prompt("Enter a color");
 let output = name + surname + color + '21';
 console.log(output);
 
+let firstNumber = parseInt(prompt("Enter a number"));
+let secondNumber = parseInt(prompt("Enter a second number"));
+output = name + surname + color + (firstNumber / secondNumber);
+console.log(output);

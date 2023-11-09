f38d33c599f891610a6a76ae446b28cee1bed069
CampanelliEmanuele <emanuele.campanelli2001@gmail.com>
2023-11-09 14:23:45 +0100

    Base exercise completed

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+let name = prompt("Enter your name");
+let surname = prompt("Enter your surname");
+let color = prompt("Enter a color");
+
+let output = name + surname + color + '21';
+console.log(output);
+

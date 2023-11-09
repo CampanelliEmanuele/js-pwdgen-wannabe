35fe64f754e55a64db214f56ec94915227f26e0c
CampanelliEmanuele <emanuele.campanelli2001@gmail.com>
2023-11-09 15:43:59 +0100

    js updated

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
-/*
 
 let name = prompt("Enter your name");
 let surname = prompt("Enter your surname");
 let color = prompt("Enter a color");
 
-let output = name + ' ' + surname + ' ' + color + ' ' + '21';
+let password = name + ' ' + surname + ' ' + color + ' ';
+
+let output = password + '21';
 console.log(output);
 document.getElementById("baseExercise").innerHTML = output;
 
 let firstNumber = parseInt(prompt("Enter a number"));
 let secondNumber = parseInt(prompt("Enter a second number"));
-output = name + ' ' + surname + ' ' + color + ' ' + (firstNumber / secondNumber);
+output = password + (firstNumber / secondNumber);
 console.log(output);
 document.getElementById("bonus1").innerHTML = output;
 
 /* Generate a random integer between 1 and 1000 */
 let casualNumber = Math.floor(Math.random() * 1001);
 
-output = name + ' ' + surname + ' ' + color + ' ' + casualNumber;
+output = password + casualNumber;
 console.log(output);
 document.getElementById("bonus4").innerHTML = output;
-
-* /
